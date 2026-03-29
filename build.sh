2bf1a1fbb2e78f75a358fa550574e72fe76d5b77
K H Arjun <kharjun48@gmail.com>
2026-03-29 20:55:16 +0530

    Forced stable Python environment and pre-installed build tools

--- build.sh
+++ build.sh
@@ -12,8 +12,15 @@ echo "Moving static files to backend..."
 rm -rf backend/static
 cp -r out backend/static
 
-echo "Installing Python dependencies..."
+echo "Environment check:"
+python --version
+python -m pip --version
+
+echo "Installing Python build tools..."
 python -m pip install --upgrade pip
+python -m pip install setuptools wheel
+
+echo "Installing Python dependencies..."
 pip install -r requirements.txt
 
 echo "Downloading NLTK data..."
